fd4755973da420d49d597a37dac8144ff58348e1
christophe <christophe@hooraylabs.com>
2019-08-09 00:50:50 +0200

    feat(checkbox): add checkbox component and story (#20)
    * feat(checkbox): add checkbox component and story
    
    * fix(label): fix label line height
    
    * feat(checkbox): add better style management add fontweight

---
+++ packages/core/src/checkbox/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {CheckBox} from '.';
+
+export default {
+  title: 'Design System|Checkbox',
+  component: CheckBox,
+};
+
+export const TextStory = () => <CheckBox label="label" name="test" />;
