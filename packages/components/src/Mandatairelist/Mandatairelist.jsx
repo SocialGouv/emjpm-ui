107096fb3994df49a8a1b45fa88f1dd7569d2861
kwiss <christophe@hooraylabs.com>
2019-08-22 10:44:16 +0200

    feat(mandataires-list): add icon to mandataire list

--- packages/components/src/Mandatairelist/Mandatairelist.jsx
+++ packages/components/src/Mandatairelist/Mandatairelist.jsx
@@ -1,5 +1,7 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Box, Flex } from 'rebass';
+import { Female, Male } from 'styled-icons/fa-solid';
 
 import { Button } from '../../../core/src/Button';
 import { Card } from '../../../core/src/Card';
@@ -63,54 +65,54 @@ const cardStyle = {
 };
 
 const Mandatairelist = (props) => {
+  const { isWoman } = props;
   return (
     <Card sx={cardStyle} width="100%">
       <Box sx={decorationStyle} />
       <Flex sx={MandatairelistStyle} {...props}>
-        <Box>
-          <Flex sx={columnStyle}>
-            <Text sx={titleStyle}>
-              Jeanne Lerry <Text sx={statusStyle}>disponible</Text>
-            </Text>
-            <Text sx={subtitleStyle}>Individuel</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex sx={columnStyle}>
-            <Text sx={labelStyle}>Commune</Text>
-            <Text sx={descriptionStyle}>Arras</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex sx={columnStyle}>
-            <Text sx={labelStyle}>Commune</Text>
-            <Text sx={descriptionStyle}>Arras</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex sx={columnStyle}>
-            <Text sx={labelStyle}>Commune</Text>
-            <Text sx={descriptionStyle}>Arras</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex sx={columnStyle}>
-            <Text sx={labelStyle}>Commune</Text>
-            <Text sx={descriptionStyle}>Arras</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Flex sx={columnStyle}>
-            <Text sx={labelStyle}>Commune</Text>
-            <Text sx={descriptionStyle}>Arras</Text>
-          </Flex>
-        </Box>
+        <Flex sx={{ color: 'black', width: '32px' }} alignItems="center" justifyContent="center">
+          <Box>{isWoman ? <Female size="32" /> : <Male size="32" />}</Box>
+        </Flex>
+        <Flex sx={columnStyle}>
+          <Text sx={titleStyle}>
+            Jeanne Lerry <Text sx={statusStyle}>disponible</Text>
+          </Text>
+          <Text sx={subtitleStyle}>Individuel</Text>
+        </Flex>
+        <Flex sx={columnStyle}>
+          <Text sx={labelStyle}>Ville</Text>
+          <Text sx={descriptionStyle}>Lyon</Text>
+        </Flex>
+        <Flex sx={columnStyle}>
+          <Text sx={labelStyle}>Distance</Text>
+          <Text sx={descriptionStyle}>12 km</Text>
+        </Flex>
+        <Flex sx={columnStyle}>
+          <Text sx={labelStyle}>Dispo</Text>
+          <Text sx={descriptionStyle}>15</Text>
+        </Flex>
+        <Flex sx={columnStyle}>
+          <Text sx={labelStyle}>Email</Text>
+          <Text sx={descriptionStyle}>johnhenry@gmail.com</Text>
+        </Flex>
+        <Flex sx={columnStyle}>
+          <Text sx={labelStyle}>Téléphone</Text>
+          <Text sx={descriptionStyle}>0683968574</Text>
+        </Flex>
         <Box>
           <Button variant="outline">Voir le cv</Button>
         </Box>
       </Flex>
     </Card>
   );
+};
+
+Mandatairelist.propTypes = {
+  isWoman: PropTypes.bool,
+};
+
+Mandatairelist.defaultProps = {
+  isWoman: false,
 };
 
 export { Mandatairelist };
