df2ebba706fe740667dfb4475b5b15d35119f04d
Rémi Roycourt <remiroycourt@gmail.com>
2020-03-17 11:21:30 +0100

    feat(mandataire): add cusor to list items

--- packages/components/src/MandataireListItem/style.js
+++ packages/components/src/MandataireListItem/style.js
@@ -97,10 +97,11 @@ const cardStyle = {
   '&:hover': {
     bg: 'cardSecondary',
   },
+  cursor: "pointer",
   mb: '1',
   overflow: 'hidden',
   pl: '16px',
-  position: 'relative',
+  position: 'relative'
 };
 
 export {
